bebc90ba8e4b80975a9f7c9b377d459deccaf90c
mojtaba <mojtaba.kh.sh@gmail.com>
2021-01-15 16:56:05 +0330

    create BookableFactory , BookableSeeder

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bookable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookableFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bookable::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name,
+            'description' => $this->faker->text,
+        ];
+    }
+}
--- database/seeders/BookableSeeder.php
+++ database/seeders/BookableSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bookable;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(BookableSeeder::class);
+        Bookable::factory()->count(100)->create();
     }
 }
