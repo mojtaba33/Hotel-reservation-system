e1ccb92b357965e0fa7393d99f70d7596cf13ed3
mojtaba <mojtaba.kh.sh@gmail.com>
2021-01-20 19:00:40 +0330

    modify CheckBookingAvailability.php

--- app/Http/Controllers/api/CheckBookingAvailability.php
+++ app/Http/Controllers/api/CheckBookingAvailability.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bookable;
 use Illuminate\Http\Request;
 
 class CheckBookingAvailability extends Controller
@@ -10,11 +11,26 @@ class CheckBookingAvailability extends Controller
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Bookable $bookable
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Bookable $bookable , Request $request)
     {
+        $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:today',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from',
+        ]);
 
+        $bookings = $bookable->bookings()
+            ->where('to','>=',$request->input('from'))
+            ->where('from','<=',$request->input('to'))
+            ->get()->count();
+
+        $is_available = $bookings === 0 ;
+
+        return response()->json([
+            'data' => ['is_available' => $is_available]
+        ]);
     }
 }
