1d5fc4241484d778bb724e7f769ca97800a65ec2
mojtaba <mojtaba.kh.sh@gmail.com>
2021-01-22 16:46:31 +0330

    add UsesUuid.php trait + use it in Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    use HasFactory;
+    use HasFactory , UsesUuid;
 
     public function booking()
     {
