16cb78c9bca54c9cd58c06ea712840d306d73c83
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-07 17:28:01 +0330

    add Auth controllers & routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,32 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
 window.Vue = require('vue').default;
 
-import VueRouter from 'vue-router';
-import router from './routes';
-import {store} from './store';
-import moment from 'moment';
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
-import Index from "./Index";
-import StarRating from "./components/global/StarRating";
-import Success from "./components/global/Success";
-import ValidationErrors from "./components/global/ValidationErrors";
-import FatalError from "./components/global/FatalError";
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.filter('fromNow', function (value) {
-    return moment(value).fromNow();
-});
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-Vue.component("star-rating", StarRating);
-Vue.component("success", Success);
-Vue.component("validation-errors", ValidationErrors);
-Vue.component("fatal-error", FatalError);
-
-Vue.use(VueRouter);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
 const app = new Vue({
     el: '#app',
-    router,
-    store,
-    components:{
-        Index
-    }
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{all?}', function () {
     return view('welcome');
 })->where('all','^(?!api\/)[\/\w\.\,-]*');
+
+Auth::routes();
