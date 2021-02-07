fa3a3991de41b453b569e33be668c7f230ce115b
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-07 17:54:31 +0330

    create Login.vue

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -4,12 +4,15 @@ import Bookable from "./components/Bookables/Bookable";
 import ShowBookable from "./components/Bookable/ShowBookable";
 import Review from "./components/Review/Review";
 import Basket from "./components/Basket/Basket";
+import Login from "./components/Auth/Login";
 
 const routes = [
     { path: '/', component: Bookable , name : "home" },
     { path: '/bookable/:slug', component: ShowBookable , name : "bookable-show" },
     { path: '/review/:id', component: Review , name : "review-show" },
     { path: '/basket', component: Basket , name : "basket" },
+
+    { path: '/login', component: Login , name : "login" },
 ];
 
 const router = new VueRouter({
