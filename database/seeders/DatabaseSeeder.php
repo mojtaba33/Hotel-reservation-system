dd302314927053d100f5a905ed2634c83905037f
mojtaba <mojtaba.kh.sh@gmail.com>
2021-02-07 19:09:57 +0330

    create UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UserSeeder::class);
         $this->call(BookableSeeder::class);
         $this->call(BookingSeeder::class);
         $this->call(ReviewSeeder::class);
